c155f261bc5d58a4baa265e00eaa4f8a84067224
AnuragDubey67 <101169657+AnuragDubey67@users.noreply.github.com>
2024-08-12 17:39:16 +0530

    error fix

--- src/components/common/AlertConfirmation.tsx
+++ src/components/common/AlertConfirmation.tsx
@@ -1,54 +1,3 @@
-// import React from 'react';
-// import {
-//   AlertDialog,
-//   AlertDialogAction,
-//   AlertDialogCancel,
-//   AlertDialogContent,
-//   AlertDialogDescription,
-//   AlertDialogFooter,
-//   AlertDialogHeader,
-//   AlertDialogTitle,
-//   AlertDialogTrigger,
-// } from '../ui/alert-dialog';
-
-// interface Props {
-//   title: React.ReactNode;
-//   description?: React.ReactNode;
-//   cancelButtonText?: string;
-//   confirmButtonText?: string;
-//   onConfirm: () => void;
-//   children: React.ReactNode;
-// }
-
-// export default function AlertConfirmation({
-//   title,
-//   description,
-//   cancelButtonText = 'Cancel',
-//   confirmButtonText = 'Confirm',
-//   onConfirm,
-//   children,
-// }: Props) {
-//   return (
-//     <AlertDialog>
-//       <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
-//       <AlertDialogContent>
-//         <AlertDialogHeader>
-//           <AlertDialogTitle>{title}</AlertDialogTitle>
-//           <AlertDialogDescription>{description}</AlertDialogDescription>
-//         </AlertDialogHeader>
-//         <AlertDialogFooter>
-//           <AlertDialogCancel>{cancelButtonText}</AlertDialogCancel>
-//           <AlertDialogAction
-//             className="bg-destructive hover:bg-destructive/90"
-//             onClick={onConfirm}
-//           >
-//             {confirmButtonText}
-//           </AlertDialogAction>
-//         </AlertDialogFooter>
-//       </AlertDialogContent>
-//     </AlertDialog>
-//   );
-// }
 
 import React from 'react';
 import { AlertDialog ,
@@ -61,17 +10,7 @@ import { AlertDialog ,
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '../ui/ui/alert-dialog';
-// import {
-//   AlertDialog,
-//   AlertDialogAction,
-//   AlertDialogCancel,
-//   AlertDialogContent,
-//   AlertDialogDescription,
-//   AlertDialogFooter,
-//   AlertDialogHeader,
-//   AlertDialogTitle,
-//   AlertDialogTrigger,
-// } from '../../ui/ui/alert-dialog';
+
 
 interface Props {
   title: React.ReactNode;
